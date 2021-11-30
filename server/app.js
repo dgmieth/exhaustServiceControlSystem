09db9e1a0c8d6e6a1c16a057dd961edfc97a9d35
dgmieth <diegombb@bb.com.br>
2021-11-30 14:22:22 -0300

    second commit

--- server/app.js
+++ server/app.js
@@ -16,7 +16,6 @@ app.use((req,res,next)=> {
 app.use(express.urlencoded());
 app.use(express.json())
 app.use((req,res,next)=> {
-    console.log('passed')
     next()
 })
 // insert new protocolo
@@ -24,11 +23,7 @@ app.post('/insertProt',(req,res,next)=>{
     Info.newProtocolo(req.body.prot)
     .then(([data,meta])=>{
         if(data){
-            Info.getProtocolos()
-            .then(([data,meta])=> {
-                //console.log(data)
-                res.json(sortProtocolos(data))
-            })
+            returnAllProts(req,res,next)
         }else{
             res.json({error: 'Not possible to save protocol'})
         }
@@ -38,18 +33,10 @@ app.post('/insertProt',(req,res,next)=>{
     ])
 })
 app.post('/delProt',(req,res,next)=>{
-    console.log(req.body)
     Info.deleteProtocolo(req.body.id, req.body.just)
     .then(([data,meta])=>{
         if(data){
-            Info.getProtocolos()
-            .then(([data1,meta])=> {
-                // console.log(data1)
-                res.json(sortProtocolos(data1))
-            })
-            .catch(err => {
-                res.json({error: err})
-            })
+            returnAllProts(req,res,next)
         }else{
             res.json({error: 'Not possible to save protocol'})
         }
@@ -59,38 +46,21 @@ app.post('/delProt',(req,res,next)=>{
     ])
 })
 app.post('/insertInfoProt',(req,res,next)=> {
-    console.log(req.body)
     if(req.body.type==='addUser'){
         Info.insertUser(req.body.protId,req.body.name,req.body.mci,req.body.cpfCnpj)
         .then(([data,meta])=>{
             if(data){
-                Info.getProtocolos()
-                .then(([data1,meta])=> {
-                    //console.log(data1)
-                    res.json(sortProtocolos(data1))
-                })
-                .catch(err => {
-                    res.json({error: err})
-                })
+                returnAllProts(req,res,next)
             }
         })
         .catch(err => {
             res.json({error: err})
         })
     }else if (req.body.type==='addNote'){
-        console.log(123)
         Info.insertNote(req.body.protId,req.body.titulo,req.body.information)
         .then(([data,meta])=>{
-            console.log(data)
             if(data){
-                Info.getProtocolos()
-                .then(([data1,meta])=> {
-                    //console.log(data1)
-                    res.json(sortProtocolos(data1))
-                })
-                .catch(err => {
-                    res.json({error: err})
-                })
+                returnAllProts(req,res,next)
             }
         })
         .catch(err => {
@@ -101,19 +71,11 @@ app.post('/insertInfoProt',(req,res,next)=> {
 })
 //
 app.post('/delInfoProt',(req,res,next)=> {
-    console.log(req.body)
     if(req.body.type==='delUser'){
         Info.deletetUser(req.body.pessoaId)
         .then(([data,meta])=>{
             if(data){
-                Info.getProtocolos()
-                .then(([data1,meta])=> {
-                    //console.log(data1)
-                    res.json(sortProtocolos(data1))
-                })
-                .catch(err => {
-                    res.json({error: err})
-                })
+                returnAllProts(req,res,next)
             }
         })
         .catch(err => {
@@ -123,14 +85,18 @@ app.post('/delInfoProt',(req,res,next)=> {
         Info.delNote(req.body.observationId)
         .then(([data,meta])=>{
             if(data){
-                Info.getProtocolos()
-                .then(([data1,meta])=> {
-                    //console.log(data1)
-                    res.json(sortProtocolos(data1))
-                })
-                .catch(err => {
-                    res.json({error: err})
-                })
+                returnAllProts(req,res,next)
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.json({error: err})
+        })
+    }else if (req.body.type==='finishProt'){
+        Info.finishProtocolo(req.body.protId)
+        .then(([data,meta])=>{
+            if(data){
+                returnAllProts(req,res,next)
             }
         })
         .catch(err => {
@@ -139,23 +105,49 @@ app.post('/delInfoProt',(req,res,next)=> {
         })
     }
 })
-//get allProtocolos
-app.get('/',(req,res,next)=>{
-    Info.getProtocolos()
-    .then(([data,meta])=> {
-        //console.log(data)
-        console.log(sortProtocolos(data))
-        res.json(sortProtocolos(data))
+app.get('/tasks',(req,res,next)=>{
+    console.log(req.query)
+    console.log('GET -> you reached /tasks')
+    Info.getTasks(parseInt(req.query.protId))
+    .then(([tasks,tasksMeta])=>{
+        if(tasks){
+            return res.json({data:sortTasks(tasks)})
+        }else{
+            return res.json({error:'not possible to retrieve information'})
+        }
     })
     .catch(err => {
-        res.json({error: err})
+        console.log(err)
+        return res.json({error:'not possible to retrieve information'})
     })
+})
+app.post('/tasks',(req,res,next)=>{
+    console.log('POST -> you reached /tasks')
+    console.log(req.body)
+    if(req.body.type==='newTask'){
+        Info.newTask(req.body.protId,req.body.taskType,req.body.info)
+        .then(([newTask, newTaskMetada])=>{
+            res.json(newTask)
+        })
+        .catch(err => {
+            res.json({error:err})
+        })
+    }else{
+        res.json({sucess:'you reached /tasks'})
+    }
+})
+//get allProtocolos
+app.get('/',(req,res,next)=>{
+    returnAllProts(req,res,next)
 })
 
 app.listen(3001,()=>{
     console.log('server on port 3001')
 })
-
+/*========================================
+            PROTOCOLOS
+  ========================================
+*/
 function sortProtocolos(data){
     var returnData = []
     var protId = 0
@@ -167,7 +159,8 @@ function sortProtocolos(data){
                 id:p.id, 
                 concluido:p.concluido,
                 excluido:p.excluido,
-                just:p.excluidojustification
+                just:p.excluidojustification,
+                insertedin:p.insertedin
             }
             var pdFiltered = data.filter(pr => {
                 if(pr.id === protId){
@@ -211,8 +204,58 @@ function sortProtocolos(data){
             object['people'] = people
             object['observation'] = observation
             returnData.push(object)
-            console.log(observation)
         }
     })
     return returnData
+}
+function returnAllProts(req,res,next) {
+    Info.getProtocolos()
+    .then(([data1,meta])=> {
+        res.json(sortProtocolos(data1))
+    })
+    .catch(err => {
+        res.json({error: err})
+    })
+}
+/*========================================
+            TASKS
+  ========================================
+*/
+function sortTasks(data){
+    var taskId = 0
+    var returnArray = []
+    data.forEach(d => {
+        if(d.id !== taskId){
+            taskId = d.id
+            var people = []
+            var filtered = data.filter(d=> {if(d.id === taskId){return true}})
+            filtered.forEach(d => {
+                people.push({
+                    peopleName:	d.peopleName,
+                    peopleMci:d.peopleMci,
+                    peopleCpfCnpj: 	d.peopleCpfCnpj,
+                    assetownershipratio:d.assetownershipratio
+                })
+            })
+            returnArray.push({
+                familyGroup:d.familyGroup,
+                familyGroupId:d.familyGroupId,
+                classificationGroup	:d.classificationGroup,
+                classificationGroupId	:d.classificationGroupId,
+                id	:d.id,
+                grouptasks_fk	:d.grouptasks_fk,
+                protocolo_fk	:d.protocolo_fk,
+                people_fk	:d.people_fk,
+                value	:d.value,
+                createdin	:d.createdin,
+                excluded	:d.excluded,
+                excludedin	:d.excludedin,
+                exckludedjustification	:d.exckludedjustification,
+                finished	:d.finished,
+                finishedin	:d.finishedin,
+                people:people
+            })
+        }
+    })
+    return returnArray
 }
--- server/model/Info.js
+++ server/model/Info.js
@@ -9,14 +9,23 @@ module.exports = class Info {
 
 //CLASS methods
     static getProtocolos(){
-       return db.query(`SELECT t.*, m.name,m.cpf_cnpj,m.mci,m.id as pessoaId, n.chave, n.value,n.id as observationId
+       return db.query(`SELECT t.*, m.name,m.cpf_cnpj,m.mci,m.id as pessoaId, 
+                                n.chave, n.value,n.id as observationId
                         FROM protocolos t 
                         LEFT JOIN (SELECT e.* FROM people e WHERE e.excluded = 0) m ON m.protocolo_fk = t.id
                         LEFT JOIN (SELECT x.* FROM protobservation x WHERE x.excluded = 0) n ON n.prot_fk = t.id
+                        ORDER BY t.insertedin DESC
                         `)
     }
     static newProtocolo(prot){
         return db.query(`INSERT INTO protocolos (protocolos) VALUES (?)`,[`${prot}`])
+    }
+    static finishProtocolo(prot){
+        return db.query(`UPDATE protocolos 
+                            SET 
+                                concluido = 1,
+                                concluidoem = current_timeStamp()
+                            WHERE id = ?`,[`${prot}`])
     }
     static deleteProtocolo(protId,justification){
         return db.query(`UPDATE protocolos
@@ -48,5 +57,27 @@ module.exports = class Info {
                                 excludedin = current_timeStamp()
                             WHERE id = ?
                             `,[observationId])
+    }
+    static newTask(protId,taksGroupId,value){
+        return db.query(`INSERT INTO tasks(grouptasks_fk,protocolo_fk,value) 
+                            VALUES(?,?,?)`,[`${taksGroupId}`,`${protId}`,`${value}`])
+    }
+    static getTasks(protocolo){
+        return db.query(`
+        SELECT 
+            f.name as familyGroup,
+            f.id as familyGroupId,
+            ct.nome as classificationGroup,
+            ct.id as classificationGroupId,
+            p.name as peopleName,
+            p.mci as peopleMci,
+            p.cpf_cnpj as peopleCpfCnpj,
+            t.*
+        FROM exaustaoservice.tasks t 
+        left join exaustaoservice.taskgroup tg on tg.id = t.grouptasks_fk 
+        left join exaustaoservice.people p on p.id = t.people_fk 
+        left join exaustaoservice.classificationtasks ct on ct.id = tg.classification_fk 
+        left join exaustaoservice.familiatasks f on f.id = tg.family_fk 
+        where t.protocolo_fk = ?`,[`${protocolo}`])
     }
 }
